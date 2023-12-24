ceb5c3d1a37b4f80639bfd336586fc870e81cc45
alfarasjb <72119101+alfarasjb@users.noreply.github.com>
2023-12-24 20:49:46 +0800

    update

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name = 'tradetestlib',
+    description = 'A backtesting library for MetaTrader5',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    version = '0.1.3',
+    author = 'Jay Alfaras',
+    author_email='alfarasjb@gmail.com',
+    url = 'https://github.com/alfarasjb/TradeTestLib',
+    packages = find_packages(),
+    install_requires = [
+        'numpy>=1.21.2',
+        'pandas>=1.4.4',
+        'matplotlib',
+        'seaborn',
+        'tqdm',
+        'MetaTrader5'
+    ],
+    include_package_data=True,
+    python_requires = '>=3.8'
+)
